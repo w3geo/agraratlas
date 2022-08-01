60eb05e26d8f60449057857c8813e56daab5d3f1
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-08-01 10:15:11 +0200

    Change basemap styles by zoom

--- src/composables/useMap.js
+++ src/composables/useMap.js
@@ -15,7 +15,7 @@ const extent = [9.33583, 46.08870, 17.42424, 49.36705];
 const basemapAttribution = 'Grundkarte: <a href="https://basemap.at/">basemap.at</a>';
 
 const basemap = new TileLayer({
-  maxZoom: 14,
+  maxZoom: 16,
   source: new XYZ({
     url: 'https://maps.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png',
     maxZoom: 19,
@@ -25,7 +25,7 @@ const basemap = new TileLayer({
 });
 
 const basemapOrtho = new TileLayer({
-  minZoom: 14,
+  minZoom: 16,
   source: new XYZ({
     url: 'https://maps.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg',
     maxZoom: 19,
