ee37dd191fc82f83ba46d8da9613889c49beba66
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-01-29 17:15:44 +0100

    Faster inspire to local id conversion

--- scripts/inspireid-to-localid.js
+++ scripts/inspireid-to-localid.js
@@ -1,17 +1,9 @@
 import { createReadStream, createWriteStream, existsSync } from 'fs';
+import { Transform } from 'stream';
 import esMain from 'es-main';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { chain } from 'stream-chain';
-import streamJson from 'stream-json';
-import Pick from 'stream-json/filters/Pick.js';
-import StreamArray from 'stream-json/streamers/StreamArray.js';
-import { Writable } from 'stream';
 import { extractInspireIdTemplate } from './extract-inspire-id.js';
-
-const { parser } = streamJson;
-const { pick } = Pick;
-const { streamArray } = StreamArray;
 
 const { argv } = yargs(hideBin(process.argv))
   .option('infile', {
@@ -31,6 +23,57 @@ const { argv } = yargs(hideBin(process.argv))
 const { infile } = argv;
 const { outfile } = argv;
 
+class RegexTransform extends Transform {
+  constructor() {
+    super();
+    this.tail = '';
+    // Match "inspire_id": "url"
+    // Capture group 1 is the URL
+    this.regex = /"inspire_id"\s*:\s*"([^"]+)"/g;
+  }
+
+  _transform(chunk, encoding, callback) {
+    const data = this.tail + chunk.toString();
+
+    // Ensure we don't process the very end of the string if it looks like it could be
+    // the start of our pattern by using a safe cut-off index.
+    const safeEndInfo = this.findSafeEnd(data);
+    const textToProcess = data.slice(0, safeEndInfo);
+    this.tail = data.slice(safeEndInfo);
+
+    const processed = textToProcess.replace(this.regex, (match, url) => {
+      const parts = url.split('/');
+      // The localID is usually the second to last part
+      // e.g. .../elu.ExistingLandUseObject/123456/MFA-2025
+      const localID = parts[parts.length - 2];
+      return `"localID": "${localID}"`;
+    });
+
+    this.push(processed);
+    callback();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  findSafeEnd(data) {
+    const tailLength = 1000;
+    if (data.length <= tailLength) return 0; // Too short to process safely, keep all as tail
+    return data.length - tailLength;
+  }
+
+  _flush(callback) {
+    // Process the remaining tail
+    if (this.tail) {
+      const processed = this.tail.replace(this.regex, (match, url) => {
+        const parts = url.split('/');
+        const localID = parts[parts.length - 2];
+        return `"localID": "${localID}"`;
+      });
+      this.push(processed);
+    }
+    callback();
+  }
+}
+
 const main = async () => {
   if (!existsSync(infile)) {
     console.error(`Input file ${infile} does not exist.`); // eslint-disable-line no-console
@@ -46,43 +89,21 @@ const main = async () => {
     process.exit(1);
   }
 
-  const outputStream = createWriteStream(outfile);
-  outputStream.write('{"type": "FeatureCollection","features":[');
+  const readStream = createReadStream(infile);
+  const writeStream = createWriteStream(outfile);
 
-  let isFirst = true;
+  const transform = new RegexTransform();
 
-  const pipeline = chain([
-    createReadStream(infile),
-    parser(),
-    pick({ filter: 'features' }),
-    streamArray(),
-    new Writable({
-      objectMode: true,
-      write({ value }, encoding, callback) {
-        const inspireIdParts = value.properties.inspire_id.split('/');
-        const localID = inspireIdParts[inspireIdParts.length - 2];
-
-        value.properties.localID = localID;
-        delete value.properties.inspire_id;
-        if (!isFirst) {
-          outputStream.write(',\n');
-        }
-        isFirst = false;
-        outputStream.write(JSON.stringify(value));
-        callback();
-      },
-    }),
-  ]);
-
-  pipeline.on('end', () => {
-    outputStream.write(']}');
-    outputStream.end();
-  });
-
-  pipeline.on('error', (err) => {
-    console.error('Pipeline failed.', err); // eslint-disable-line no-console
-    process.exit(1);
-  });
+  readStream
+    .pipe(transform)
+    .pipe(writeStream)
+    .on('finish', () => {
+      console.log(`Successfully converted ${infile} -> ${outfile}`); // eslint-disable-line no-console
+    })
+    .on('error', (err) => {
+      console.error('Pipeline failed.', err); // eslint-disable-line no-console
+      process.exit(1);
+    });
 };
 
 export default main;
